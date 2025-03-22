bcb446b54caaffaf905a40647234094975987406
Roman Rahman <rahman.roman@gmail.com>
2025-03-22 16:11:55 +0200

    More telling formatting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const data = `city,population,area,density,country
   New York City,8537673,784,10892,United States
   Bangkok,8280925,1569,5279,Thailand`;
 
-const formatting = [[18, false], [10, true], [8, true], [8, true], [18, true], [6, true]];
+const formatting = [[18, 'padEnd'], [10, 'padStart'], [8, 'padStart'], [8, 'padStart'], [18, 'padStart'], [6, 'padStart']];
 
 if (data) {
   const [table, max] = data
@@ -30,7 +30,7 @@ if (data) {
     .map(row => [...row, Math.round((row[3] * 100) / max)])
     .sort((r1, r2) => (r2[5] > r1[5] ? 1 : -1))
     .map(row => row
-      .map((cell, index) => String(cell)[formatting[index][1] ? 'padStart' : 'padEnd'](formatting[index][0]))
+      .map((cell, index) => String(cell)[formatting[index][1]](formatting[index][0]))
       .join(''))
     .forEach(line => console.log(line));
 }
